906cd959b1fdeb13278c851989dcbe9ee7a57473
asqaria <asqaria@hotmail.com>
2025-02-21 22:26:49 +0500

    add layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,29 @@
 import { ClerkProvider } from "@clerk/nextjs";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description: "developers helper",
+  icons: {
+    icon: "favicon.ico",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
   children,
@@ -8,9 +31,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">Hello world!</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
